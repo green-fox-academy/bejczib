ff83a89b019e3d49a6738ed10342ad9224324b5b
Balint Bejczi <bejczi.balint@gmail.com>
2015-12-01 09:58:23 +0100

    add first day practise

--- week-4/day-1/bank.py
+++ week-4/day-1/bank.py
@@ -12,8 +12,17 @@ class BankAccount(object):
     def print_balance(self):
         return self.name, self.ballance
 
-client1 = BankAccount('Balint', 100)
+    def transfer(self, target, money):
+        try:
+            if self.ballance > money:
+                target.receive(money)
+                self.pay(money)
+            else:
+                raise Exception('Nagy turot Oreg, nincs loveeee!')
+        except Exception:
+                print('Nagy turot Oreg, nincs annyi penzed!')
 
-client1.pay(20)
-client1.receive(50)
-print(client1.print_balance())
+client1 = BankAccount('Balint', 0)
+client2 = BankAccount('Feri', 500)
+
+client1.transfer( client2, 50)
---
+++ week-4/day-1/rpg.py
@@ -0,0 +1,50 @@
+import random
+
+class Character:
+    def __init__(self, name, HP, damage):
+        self.name = name
+        self.HP = HP
+        self.damage = damage
+
+    def print_status(self):
+        if self.HP <= 0:
+            return self.name + ' is dead.'
+        else:
+            return self.name + ' HP is ' + str(self.HP)
+
+    def drink_potion(self):
+        potion = 10
+        self.HP += potion
+
+    def strike(self, target):
+        target.HP -= self.damage
+
+    def round(self):
+        round = 0
+        while en.HP >=0 and te.HP >=0 and arch_enemy.HP >= 0:
+            en.strike(arch_enemy)
+            te.strike(arch_enemy)
+            arch_enemy.strike(en)
+            arch_enemy.strike(te)
+            round += 1
+        if en.HP <= 0 or te.HP <= 0 or arch_enemy.HP <= 0:
+            print('game over')
+            print(en.print_status())
+            print(te.print_status())
+            print(arch_enemy.print_status())
+            print('{} kor allatt basztuk szet oket'.format(round))
+
+
+enemies = [
+    Character('Bela', random.randint(50,100), random.randint(5,10)),
+    Character('Judit', random.randint(50,100), random.randint(5,10)),
+    Character('Gyuri', random.randint(50,100), random.randint(5,10)),
+    Character('Tojas', random.randint(50,100), random.randint(5,10))
+]
+
+arch_enemy = random.choice(enemies)
+
+en = Character('Balint', 100, 10)
+te = Character('Peti', 100, 10)
+
+en.round()
