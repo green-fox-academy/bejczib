f85c5d418220ea5e6d9be9e7b45ac2030abb0bdb
Balint Bejczi <bejczi.balint@gmail.com>
2015-12-11 17:14:26 +0100

    add ballgame

--- week-5/day-5/list_test.py
+++ week-5/day-5/list_test.py
@@ -4,16 +4,16 @@ from list import MyMagic
 class ListTest(unittest.TestCase):
 
     def setUp(self):
-      self.names = [{ 'id': 1, 'name': 'John'},
+        self.names = [{ 'id': 1, 'name': 'John'},
                       { 'id': 2, 'name': 'Molly'},
                       { 'id': 3, 'name': 'Jane'}]
 
     def test_array_of_names(self):
-      my_list = MyMagic(self.names)
-      self.assertEqual(my_list.array_of_names(), ['John', 'Molly', 'Jane'])
+        my_list = MyMagic(self.names)
+        self.assertEqual(my_list.array_of_names(), ['John', 'Molly', 'Jane'])
 
     def test_name_starts_with_j(self):
-      my_list = MyMagic(self.names)
-      self.assertEqual(my_list.names_with_j(), ['John', 'Jane'])
+        my_list = MyMagic(self.names)
+        self.assertEqual(my_list.names_with_j(), ['John', 'Jane'])
 
 unittest.main()
