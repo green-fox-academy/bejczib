438a6a57f183db08b50a685d22d48673117e5763
Balint Bejczi <bejczi.balint@gmail.com>
2015-12-18 12:24:04 +0100

    Andi: The list comprehension is szexy!

--- week-6/day-5/katapotter.py
+++ week-6/day-5/katapotter.py
@@ -1,10 +1,26 @@
 
 def discounter(array):
+    price = 0
+    while array != []:
+        unique = list(set(array))
+        price += amount(unique)
+        array = [i for i in array if i not in unique or unique.remove(i)]
+
+        # array2 = []
+        # for item in array:
+        #     if item in unique:
+        #         unique.remove(item)
+        #     else:
+        #         array2.append(item)
+        # array = array2
+    return price
+
+def amount(unique_list):
     discounts = [1, 0.95, 0.9, 0.8, 0.75]
-    book_types = len(set(array))
-    discount = discounts[book_types - 1]
-    full_price_book = len(array)-book_types
-    result = round(8 * (book_types * discount + full_price_book), 2)
+    books = len(unique_list)
+    discount = discounts[books - 1]
+    return books * discount * 8
+
 
 
 
