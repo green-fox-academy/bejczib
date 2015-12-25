aef0805d042fed272eaca899289e7461b8959a6f
Balint Bejczi <bejczi.balint@gmail.com>
2015-12-25 16:11:04 +0100

    add fizzbuzz.js

---
+++ holidaJS/fizzbuzz.js
@@ -0,0 +1,23 @@
+function fizzbuzz() {
+    var i = 0
+    while (i <= 100) {
+        if(i % 3 == 0 && i % 5 == 0) {
+            console.log('fizzbuzz');
+            i++
+        }
+        else if (i % 5 == 0) {
+            console.log('fizz');
+            i++
+        }
+        else if (i % 3 == 0) {
+            console.log('buzz');
+            i++
+        }
+        else {
+            console.log(i);
+            i++
+        }
+    }
+}
+
+fizzbuzz()
