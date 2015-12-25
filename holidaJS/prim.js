bc1ef44de115a60162f526b90e20f82b5543659a
Balint Bejczi <bejczi.balint@gmail.com>
2015-12-25 15:19:21 +0100

    add prim.js

---
+++ holidaJS/prim.js
@@ -0,0 +1,21 @@
+function isPrim(num) {
+    var hasDivisor = 0;
+    var i = 0;
+    while (i <= num) {
+        if (num % i ==0) {
+            hasDivisor = 1;
+            break
+        }
+        else {
+            i++
+        }
+    }
+
+    if(hasDivisor ==1 && num != 0 && num != 1) {
+        console.log("it's prim!")
+    }else {
+        console.log("it's not prim!")
+    }
+}
+
+isPrim(0)
