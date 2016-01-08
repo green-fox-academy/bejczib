623035a09d45bed51a1129047066f7f240ba1ee6
Balint Bejczi <bejczi.balint@gmail.com>
2016-01-08 12:13:17 +0100

    add demo

--- week-7/day-4/demo/script.js
+++ week-7/day-4/demo/script.js
@@ -2,9 +2,43 @@
 
 var bodyvar = document.querySelector('body');
 var pic = document.querySelector('.kep');
+var first_pic = document.querySelector('.first_pic');
 
 function changePicture(src) {
    pic.setAttribute('src', src)
+}
+
+function clickPicture(src) {
+   pic.setAttribute('src', src)
+}
+
+function nextPicture() {
+   if (picturePosition === pictures.length-1) {
+      next.style.visibility = "hidden"
+   } else {
+      changePicture(pictures[++picturePosition]);
+      prev.style.visibility = "visible";
+   }
+   console.log(picturePosition);
+      actives = container.querySelectorAll('.active')
+      for (var i=0;i<actives.length;i++) {
+         actives[i].classList.remove('active');
+      }
+      document.getElementById('pic' + picturePosition).classList.add('active')
+}
+
+function prevPicture() {
+   if(picturePosition === 0) {
+      prev.style.visibility = "hidden"
+   } else {
+      changePicture(pictures[--picturePosition]);
+      next.style.visibility = "visible";
+   }
+      actives = container.querySelectorAll('.active')
+      for (var i=0;i<actives.length;i++) {
+         actives[i].classList.remove('active');
+      }
+      document.getElementById('pic' + picturePosition).classList.add('active');
 }
 
 var pictures = [
@@ -19,16 +53,46 @@ var pictures = [
    'http://lorempixel.com/600/400/sports/10/'
 ];
 
-var picture = 0;
+var picturePosition = 0;
 var next = document.querySelector('.next');
-var previous = document.querySelector('.previous');
+var prev = document.querySelector('.previous');
+first_pic.classList.add('active');
+var container = document.querySelector('.container');
+var actives = container.querySelectorAll('.active');
 
 next.addEventListener('click',
-   function () {
-      picture === pictures.length-1 ? picture = 0 : changePicture(pictures[++picture]);
-});
+   function () {nextPicture();});
 
-previous.addEventListener('click',
-   function () {
-      picture === 0 ? picture = pictures.length-1 : changePicture(pictures[--picture]);
-});
+prev.addEventListener('click',
+   function () {prevPicture();});
+
+
+container.addEventListener('click',
+   function (event) {
+      var picture_src = event.target.getAttribute('src')
+      clickPicture(picture_src);
+      picturePosition = pictures.indexOf(picture_src);
+      actives = container.querySelectorAll('.active')
+      for (var i=0;i<actives.length;i++) {
+         actives[i].classList.remove('active');
+      }
+      event.target.classList.add('active');
+   })
+
+document.addEventListener("keypress", keyEventHandler);
+function keyEventHandler(e) {
+    var keyCode = e.keyCode;
+    console.log(e.keyCode);
+    if(keyCode === 97) {
+      if ( picturePosition === 0) {
+         picturePosition = pictures.length;
+      }
+        prevPicture();
+    } else if (keyCode == 100) {
+      if ( picturePosition === pictures.length-1) {
+         picturePosition = -1;
+      }
+      nextPicture();
+    }
+};
+
