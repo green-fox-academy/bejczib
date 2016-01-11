6c6a4e19b76279b397bd7b6a387fb4458cf92117
Balint Bejczi <bejczi.balint@gmail.com>
2016-01-11 15:04:27 +0100

    add codewars kata

--- week-7/day-4/sumbig.js
+++ week-7/day-4/sumbig.js
@@ -1,40 +1,63 @@
 'use strict';
 
 function sumStrings(a,b) {
-
-  if (a.length <= String(Number.MAX_SAFE_INTEGER).length/2) {
-    return (+a + +b).toString()
-  } else {
-    var lengthIndex = Math.round(getBigger(a,b).length/2);
-    console.log('lengthindex :' + lengthIndex);
-
-    console.log('a :' + a);
-    var aHalf1 = a.substr(0,a.length - lengthIndex);
-    var aHalf2 = a.substr(a.length -lengthIndex,a.length-1);
-    console.log('b :' + b);
-    var bHalf1 = b.substr(0,b.length -lengthIndex);
-    var bHalf2 = b.substr(b.length - lengthIndex,b.length-1);
-    var result2 = (Number(aHalf2) + Number(bHalf2)).toString();
-    var temp = result2.toString()[0]
-    var result1 = Number(aHalf1) + Number(bHalf1) + Number(temp).toString();
-    console.log('aHalf1 :' + aHalf1)
-    console.log('bHalf1 :' + bHalf1)
-    console.log('aHalf2 :' + aHalf2)
-    console.log('bHalf2 :' + bHalf2)
-    console.log('result1 :' + result1)
-    console.log('result2 :' + result2)
-    console.log('131151201344081895336534324866 <--- expected')
-    return result1 + result2.substr(1,result2.length-1);
-
-  }
+    var bigger = getBigger(a,b).length != 0 ? getBigger(a,b).split('') : ['0'];
+    var smaller = getSmaller(a,b).length != 0 ? getSmaller(a,b).split('') : ['0'];
+    var biggerIndex = bigger.length-smaller.length;
+    var temp = '';
+    var result = '';
+    for (var i=smaller.length-1;i>=0;i--) {
+        var sum = Number(temp);
+        sum += Number(smaller[i]) + Number(bigger[i+biggerIndex]);
+        temp = '';
+        smaller.splice(i, 1);
+        bigger.splice(i+biggerIndex, 1);
+        if (sum === 0 && i === 0) {sum = ''};
+        if (String(sum).length == 2 && sum != 0) {
+            temp = String(sum).charAt(0);
+            sum = String(sum).charAt(1);
+        }
+        result+= sum;
+    }
+    if (bigger.length != 0) {
+        for(var j=bigger.length-1;j>=0;j--) {
+            console.log(temp);
+            if (temp != '' ) {
+                var sum2= Number(bigger[j])+ Number(temp);
+                temp = '';
+                bigger.splice(j, 1);
+                var temp2 = '';
+                if (String(sum2).length == 2 ) {
+                    temp2 = String(sum2).charAt(0);
+                    sum2 = String(sum2).charAt(1);
+                }
+            } else  {
+                sum2 = bigger[j];
+                console.log(sum2);
+            }
+            result += sum2
+            if ( temp2 != '') {
+                console.log('cssss')
+                result += temp2;
+            }
+        }
+    } else if (bigger.length === 0 && temp != '') {
+        result += temp;
+    }
+    return result.split('').reverse().join('');
 }
-
 
 function getBigger(a,b) {
     return a.length > b.length ? a : b;
 }
 
+function getSmaller(a,b) {
+     return a.length > b.length ? b : a;
+}
+
+console.log('131151201344081895336534324866')
 console.log(sumStrings('50095301248058391139327916261','81055900096023504197206408605'));
+
 
 
 
